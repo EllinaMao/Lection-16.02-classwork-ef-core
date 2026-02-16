733baea1dff20f9cea2c28d270dc57fd614b541f
Olga <xotaoshi@gmail.com>
2026-02-16 20:52:16 +0200

    hell

--- Lection 2/Controllers/HomeController.cs
+++ Lection 2/Controllers/HomeController.cs
@@ -15,10 +15,28 @@ namespace Lection_2.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Users.ToListAsync());
+        //}
+
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(await _context.Users.ToListAsync());
+            ViewData["CurrentFilter"] = searchString;
+
+            var usersQuery = _context.Users.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                usersQuery = usersQuery.Where(u => u.Name.Contains(searchString));
+            }
+
+            var users = await usersQuery.ToListAsync();
+            return View(users);
         }
+
+
+
         public IActionResult Create()
         {
             return View();
@@ -62,6 +80,33 @@ namespace Lection_2.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                var currentUser = await _context.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (currentUser != null)
+                {
+                    return View(currentUser);
+                }
+            }
+            return NotFound();
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                User user = new User { Id = id.Value };
+                _context.Entry(user).State = EntityState.Deleted;
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return NotFound();
+        }
     }
 }
